1a77c4712d27d5678e5507a34e59e278494098f5
donghashin <shindh159@gmail.com>
2018-10-27 16:10:16 +0900

    python 기본 코드

---
+++ week2/stage1.py
@@ -0,0 +1,5 @@
+product_name = '모니터'
+price = 280000
+company = '코알라전자'
+
+print(product_name, price, company)
---
+++ week2/stage2.py
@@ -0,0 +1,37 @@
+seconds_of_a_day = 60 * 60 * 24
+# print(seconds_of_a_day)
+
+price = 160000
+quantity = 24
+discount = 0.1
+total_price = price * quantity * (1 - discount)
+# print(total_price)
+
+# print('안녕', '하이')
+
+num = abs(-10) + 5
+# print(num)
+
+len_of_text = len ('Hello World!')
+# print(len_of_text)
+
+max_val = max(15,4,30,1599)
+# print(max_val)
+
+text = 'Hello world'
+replaced_text = text.replace('Hello', 'Bye')
+# print(replaced_text)
+# print(text)
+# print(text.index('l'))
+
+eng_str = 'The Best'
+eng_str = eng_str.upper()
+# print(eng_str)
+
+spaced_text = '      a a a      '
+# print(spaced_text.strip())
+
+chaining_text = spaced_text.strip().upper().replace('A', 'E')
+# print(chaining_text)
+
+
